a3c60ceb45ba3151ec613e5ef39a1d66e0eb419d
音訊科技 <mhu@speechmessage.com.tw>
2017-08-11 17:31:52 +0800

    33333

--- ChurchReport/Models/SmallGroupData.cs
+++ ChurchReport/Models/SmallGroupData.cs
@@ -16,6 +16,8 @@ namespace ChurchReport.Models
         public String SmallGroupLeaderFullName { get; set; }
         public DateTime SundayPrayers { get; set; }
 
+        public String DataStatus { get; set; }
+
         public List<Member> members;
 
         public List<Member> Members { get => members; set => members = value; }
