c7c0b7ca4c13588711182a8341b782c00eaee444
音訊科技 <mhu@speechmessage.com.tw>
2018-06-05 10:22:38 +0800

    AAA

--- ChurchReport/Models/AppointmentsList.cs
+++ ChurchReport/Models/AppointmentsList.cs
@@ -14,14 +14,14 @@ namespace ChurchReport.Models
                 return new List<Appointment> {
                     new Appointment {
                         AppointmentId = 1,
-                        Text = "Website Re-Design Plan",
+                        Text = "503教室-白碧娥",
                         OwnerId = new int[] { 4 },
                         StartDate = new DateTime(2015, 5, 25, 9, 30, 0),
                         EndDate = new DateTime(2015, 5, 25, 11, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 2,
-                        Text = "Book Flights to San Fran for Sales Trip",
+                        Text = "親子教室-吳創然",
                         OwnerId = new int[] { 2 },
                         StartDate = new DateTime(2015, 5, 25, 12, 0, 0),
                         EndDate = new DateTime(2015, 5, 25, 13, 0, 0),
@@ -29,56 +29,56 @@ namespace ChurchReport.Models
                     },
                     new Appointment {
                         AppointmentId = 3,
-                        Text = "Install New Router in Dev Room",
+                        Text = "張全興出差至台北",
                         OwnerId = new int[] { 1 },
                         StartDate = new DateTime(2015, 5, 25, 14, 30, 0),
                         EndDate = new DateTime(2015, 5, 25, 15, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 4,
-                        Text = "Approve Personal Computer Upgrade Plan",
+                        Text = "郭寬宏牧師特會",
                         OwnerId = new int[] { 3 },
                         StartDate = new DateTime(2015, 5, 26, 10, 0, 0),
                         EndDate = new DateTime(2015, 5, 26, 11, 0, 0)
                     },
                     new Appointment {
                         AppointmentId = 5,
-                        Text = "Final Budget Review",
+                        Text = "502教室-林文也",
                         OwnerId = new int[] { 1 },
                         StartDate = new DateTime(2015, 5, 26, 12, 0, 0),
                         EndDate = new DateTime(2015, 5, 26, 13, 35, 0)
                     },
                     new Appointment {
                         AppointmentId = 6,
-                        Text = "New Brochures",
+                        Text = "同工行政會議",
                         OwnerId = new int[] { 4 },
                         StartDate = new DateTime(2015, 5, 26, 14, 30, 0),
                         EndDate = new DateTime(2015, 5, 26, 15, 45, 0)
                     },
                     new Appointment {
                         AppointmentId = 7,
-                        Text = "Install New Database",
+                        Text = "飛牛牧場一日遊",
                         OwnerId = new int[] { 2 },
                         StartDate = new DateTime(2015, 5, 27, 9, 45, 0),
                         EndDate = new DateTime(2015, 5, 27, 11, 15, 0)
                     },
                     new Appointment {
                         AppointmentId = 8,
-                        Text = "Approve New Online Marketing Strategy",
+                        Text = "領袖小組長會議",
                         OwnerId = new int[] { 3, 4 },
                         StartDate = new DateTime(2015, 5, 27, 12, 0, 0),
                         EndDate = new DateTime(2015, 5, 27, 14, 0, 0)
                     },
                     new Appointment {
                         AppointmentId = 9,
-                        Text = "Upgrade Personal Computers",
+                        Text = "白碧娥休假",
                         OwnerId = new int[] { 2 },
                         StartDate = new DateTime(2015, 5, 27, 15, 15, 0),
                         EndDate = new DateTime(2015, 5, 27, 16, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 10,
-                        Text = "Customer Workshop",
+                        Text = "蕭菀伶事假",
                         OwnerId = new int[] { 3 },
                         StartDate = new DateTime(2015, 5, 28, 11, 0, 0),
                         EndDate = new DateTime(2015, 5, 28, 12, 0, 0),
@@ -86,49 +86,49 @@ namespace ChurchReport.Models
                     },
                     new Appointment {
                         AppointmentId = 11,
-                        Text = "Prepare 2015 Marketing Plan",
+                        Text = "503教室-蕭菀伶",
                         OwnerId = new int[] { 1, 3 },
                         StartDate = new DateTime(2015, 5, 28, 11, 0, 0),
                         EndDate = new DateTime(2015, 5, 28, 13, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 12,
-                        Text = "Brochure Design Review",
+                        Text = "受洗約談預備會議",
                         OwnerId = new int[] { 4 },
                         StartDate = new DateTime(2015, 5, 28, 14, 0, 0),
                         EndDate = new DateTime(2015, 5, 28, 15, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 13,
-                        Text = "Create Icons for Website",
+                        Text = "台中牧師聯禱會",
                         OwnerId = new int[] { 3 },
                         StartDate = new DateTime(2015, 5, 29, 10, 0, 0),
                         EndDate = new DateTime(2015, 5, 29, 11, 30, 0)
                     },
                     new Appointment {
                         AppointmentId = 14,
-                        Text = "Upgrade Server Hardware",
+                        Text = "晨禱特會見證",
                         OwnerId = new int[] { 4 },
                         StartDate = new DateTime(2015, 5, 29, 14, 30, 0),
                         EndDate = new DateTime(2015, 5, 29, 16, 0, 0)
                     },
                     new Appointment {
                         AppointmentId = 15,
-                        Text = "Submit New Website Design",
+                        Text = "張全興事假",
                         OwnerId = new int[] { 1 },
                         StartDate = new DateTime(2015, 5, 29, 16, 30, 0),
                         EndDate = new DateTime(2015, 5, 29, 18, 0, 0)
                     },
                     new Appointment {
                         AppointmentId = 16,
-                        Text = "Launch New Website",
+                        Text = "郭寬宏牧師出國",
                         OwnerId = new int[] { 2 },
                         StartDate = new DateTime(2015, 5, 29, 12, 20, 0),
                         EndDate = new DateTime(2015, 5, 29, 14, 0, 0)
                     },
                     new Appointment {
                         AppointmentId = 17,
-                        Text = "Stand-up meeting",
+                        Text = "領袖同工聚餐",
                         OwnerId = new int[] { 1, 2, 3, 4 },
                         StartDate = new DateTime(2015, 5, 25, 9, 0, 0),
                         EndDate = new DateTime(2015, 5, 25, 9, 15, 0),
