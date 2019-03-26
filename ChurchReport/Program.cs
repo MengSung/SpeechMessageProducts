241ae5bda20ad58f1bdb25c9beb3a20474f8a8f2
音訊科技 <mhu@speechmessage.com.tw>
2019-03-26 11:15:16 +0800

    99999

--- ChurchReport/Program.cs
+++ ChurchReport/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 
 
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 
 namespace ChurchReport
 {
@@ -22,7 +24,23 @@ namespace ChurchReport
                            {
                                o.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(30);
                                o.Limits.MaxRequestBufferSize = null;
+                               o.Limits.MaxConcurrentConnections = 1000;
+                               o.Limits.MaxConcurrentUpgradedConnections = 1000;
                            }
+                )
+            .ConfigureKestrel((context, options) =>
+                        {
+                            options.Limits.MaxConcurrentConnections = 1000;
+                            options.Limits.MaxConcurrentUpgradedConnections = 1000;
+                            //options.Limits.MaxRequestBodySize = 10 * 1024;
+                            //options.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                            //options.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                            //options.Listen(IPAddress.Loopback, 5000);
+                            //options.Listen(IPAddress.Loopback, 5001, listenOptions =>
+                            //{
+                            //    listenOptions.UseHttps("testCert.pfx", "testPassword");
+                            //});
+                        }
                 )
             .UseContentRoot(Directory.GetCurrentDirectory())
             .UseIISIntegration()
