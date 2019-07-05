dcef94f2dd0a526b88100b91cbf624464e6e9277
音訊科技 <mhu@speechmessage.com.tw>
2019-07-05 17:14:03 +0800

    /////

--- ChurchReport/WebServiceConnector/LinePayProcessor.cs
+++ ChurchReport/WebServiceConnector/LinePayProcessor.cs
@@ -19,8 +19,8 @@ namespace ChurchReport.WebServiceConnector
         ToolUtilityClass m_ToolUtilityClass = new ToolUtilityClass("DYNAMICS365");
 
         // 客製化
-        private const String CONFIRM_URL = "https://church.speechmessage.com.tw:454/api/callback/Confirm";
-        //private const String CONFIRM_URL = "https://jesus.speechmessage.com.tw:352/api/callback/Confirm";
+        //private const String CONFIRM_URL = "https://church.speechmessage.com.tw:454/api/callback/Confirm";
+        private const String CONFIRM_URL = "https://jesus.speechmessage.com.tw:352/api/callback/Confirm";
 
         public LinePayProcessor()
         {
