fc2f5f07165cb02ab055d3dceab6f65f03e60f93
音訊科技 <mhu@speechmessage.com.tw>
2019-09-20 12:52:00 +0800

    9999

--- ChurchReport/Models/Appointment.cs
+++ ChurchReport/Models/Appointment.cs
@@ -7,7 +7,9 @@ namespace ChurchReport.Models
         public String AppointmentId { get; set; }
         public string Text { get; set; }
         public string AppointmentType { get; set; }
-        public int[] CategoryId { get; set; }
+        public int[] CategoryId { get; set; }// 約會類別
+        public int LeaveId { get; set; }// 請假人資類別
+        public int LocationId { get; set; }// 場地類別
         public int[] OwnerId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
