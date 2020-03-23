a49bc3b3db26503ae873c906e05abbdb05c2b6cb
eiahb3838ya <eiahb3838ya@gmail.com>
2020-03-23 13:21:24 +0800

    \\\\

--- ChurchReport/Controllers/HomeController.cs
+++ ChurchReport/Controllers/HomeController.cs
@@ -110,7 +110,7 @@ namespace ChurchReport.Controllers
                 if (　ContactIdString != "密碼錯誤" && ContactIdString != "系統沒有設定密碼" && ContactIdString != "帳號錯誤"　)
                 {
                     string FullName = "";
-                    if ( ContactIdString != "透過 Line Id 登入" )
+                    if ( ContactIdString != "透過Line Id 登入" )
                     {
                         #region 使用者透過網頁的帳號密碼登入，所以帳號密碼就依據使用者輸入的為準
                         Guid aContactGuid = new Guid(ContactIdString);
