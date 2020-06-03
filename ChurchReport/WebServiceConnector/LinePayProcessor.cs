a7f98568b93990140c55fb7d31e7746bc3ddc78f
音訊科技 <mhu@speechmessage.com.tw>
2020-06-03 11:40:01 +0800

    ++++

--- ChurchReport/WebServiceConnector/LinePayProcessor.cs
+++ ChurchReport/WebServiceConnector/LinePayProcessor.cs
@@ -139,6 +139,9 @@ namespace ChurchReport.WebServiceConnector
                 // 收費單實收金額
                 this.m_ToolUtilityClass.SetEntityMoneyAttribute(ref aFeeToCreated, "new_fee_really_paid", new Money(0));
 
+                // 收費單實現阿拉伯數字到大寫中文的轉換，金額轉為大寫金額
+                this.m_ToolUtilityClass.SetEntityStringAttribute(ref aFeeToCreated, "new_big_chinese_number", MoneyToChinese(DedicationModel.Amount.ToString()));
+
                 // 收費單付款方式
                 this.m_ToolUtilityClass.SetOptionSetAttribute(ref aFeeToCreated, "new_pay_way", 100000005);
 
@@ -171,6 +174,355 @@ namespace ChurchReport.WebServiceConnector
                 throw e;
             }
         }
+        #region 工具區
+        /// <summary>
+        /// 實現阿拉伯數字到大寫中文的轉換，金額轉為大寫金額
+        /// </summary>
+        /// <param name="LowerMoney"></param>
+        /// <returns></returns>
+        public string MoneyToChinese(string LowerMoney)
 
+        {
+
+            string functionReturnValue = null;
+
+            bool IsNegative = false; // 是否是負數
+
+            if (LowerMoney.Trim().Substring(0, 1) == "-")
+
+            {
+
+                // 是負數則先轉為正數
+
+                LowerMoney = LowerMoney.Trim().Remove(0, 1);
+
+                IsNegative = true;
+
+            }
+
+            string strLower = null;
+
+            string strUpart = null;
+
+            string strUpper = null;
+
+            int iTemp = 0;
+
+            // 保留兩位小數 123.489→123.49　　123.4→123.4
+
+            LowerMoney = Math.Round(double.Parse(LowerMoney), 2).ToString();
+
+            if (LowerMoney.IndexOf(".") > 0)
+
+            {
+
+                if (LowerMoney.IndexOf(".") == LowerMoney.Length - 2)
+
+                {
+
+                    LowerMoney = LowerMoney + "0";
+
+                }
+
+            }
+
+            else
+
+            {
+
+                LowerMoney = LowerMoney + ".00";
+
+            }
+
+            strLower = LowerMoney;
+
+            iTemp = 1;
+
+            strUpper = "";
+
+            while (iTemp <= strLower.Length)
+
+            {
+
+                switch (strLower.Substring(strLower.Length - iTemp, 1))
+
+                {
+
+                    case ".":
+
+                        strUpart = "圓";
+
+                        break;
+
+                    case "0":
+
+                        strUpart = "零";
+
+                        break;
+
+                    case "1":
+
+                        strUpart = "壹";
+
+                        break;
+
+                    case "2":
+
+                        strUpart = "貳";
+
+                        break;
+
+                    case "3":
+
+                        strUpart = "叄";
+
+                        break;
+
+                    case "4":
+
+                        strUpart = "肆";
+
+                        break;
+
+                    case "5":
+
+                        strUpart = "伍";
+
+                        break;
+
+                    case "6":
+
+                        strUpart = "陸";
+
+                        break;
+
+                    case "7":
+
+                        strUpart = "柒";
+
+                        break;
+
+                    case "8":
+
+                        strUpart = "捌";
+
+                        break;
+
+                    case "9":
+
+                        strUpart = "玖";
+
+                        break;
+
+                }
+
+                switch (iTemp)
+
+                {
+
+                    case 1:
+
+                        strUpart = strUpart + "分";
+
+                        break;
+
+                    case 2:
+
+                        strUpart = strUpart + "角";
+
+                        break;
+
+                    case 3:
+
+                        strUpart = strUpart + "";
+
+                        break;
+
+                    case 4:
+
+                        strUpart = strUpart + "";
+
+                        break;
+
+                    case 5:
+
+                        strUpart = strUpart + "拾";
+
+                        break;
+
+                    case 6:
+
+                        strUpart = strUpart + "佰";
+
+                        break;
+
+                    case 7:
+
+                        strUpart = strUpart + "仟";
+
+                        break;
+
+                    case 8:
+
+                        strUpart = strUpart + "萬";
+
+                        break;
+
+                    case 9:
+
+                        strUpart = strUpart + "拾";
+
+                        break;
+
+                    case 10:
+
+                        strUpart = strUpart + "佰";
+
+                        break;
+
+                    case 11:
+
+                        strUpart = strUpart + "仟";
+
+                        break;
+
+                    case 12:
+
+                        strUpart = strUpart + "億";
+
+                        break;
+
+                    case 13:
+
+                        strUpart = strUpart + "拾";
+
+                        break;
+
+                    case 14:
+
+                        strUpart = strUpart + "佰";
+
+                        break;
+
+                    case 15:
+
+                        strUpart = strUpart + "仟";
+
+                        break;
+
+                    case 16:
+
+                        strUpart = strUpart + "萬";
+
+                        break;
+
+                    default:
+
+                        strUpart = strUpart + "";
+
+                        break;
+
+                }
+
+                strUpper = strUpart + strUpper;
+
+                iTemp = iTemp + 1;
+
+            }
+
+            strUpper = strUpper.Replace("零拾", "零");
+
+            strUpper = strUpper.Replace("零佰", "零");
+
+            strUpper = strUpper.Replace("零仟", "零");
+
+            strUpper = strUpper.Replace("零零零", "零");
+
+            strUpper = strUpper.Replace("零零", "零");
+
+            strUpper = strUpper.Replace("零角零分", "整");
+
+            strUpper = strUpper.Replace("零分", "整");
+
+            strUpper = strUpper.Replace("零角", "零");
+
+            strUpper = strUpper.Replace("零億零萬零圓", "億圓");
+
+            strUpper = strUpper.Replace("億零萬零圓", "億圓");
+
+            strUpper = strUpper.Replace("零億零萬", "億");
+
+            strUpper = strUpper.Replace("零萬零圓", "萬圓");
+
+            strUpper = strUpper.Replace("零億", "億");
+
+            strUpper = strUpper.Replace("零萬", "萬");
+
+            strUpper = strUpper.Replace("零圓", "圓");
+
+            strUpper = strUpper.Replace("零零", "零");
+
+            // 對壹圓以下的金額的處理
+
+            if (strUpper.Substring(0, 1) == "圓")
+
+            {
+
+                strUpper = strUpper.Substring(1, strUpper.Length - 1);
+
+            }
+
+            if (strUpper.Substring(0, 1) == "零")
+
+            {
+
+                strUpper = strUpper.Substring(1, strUpper.Length - 1);
+
+            }
+
+            if (strUpper.Substring(0, 1) == "角")
+
+            {
+
+                strUpper = strUpper.Substring(1, strUpper.Length - 1);
+
+            }
+
+            if (strUpper.Substring(0, 1) == "分")
+
+            {
+
+                strUpper = strUpper.Substring(1, strUpper.Length - 1);
+
+            }
+
+            if (strUpper.Substring(0, 1) == "整")
+
+            {
+
+                strUpper = "零圓整";
+
+            }
+
+            functionReturnValue = strUpper;
+
+            if (IsNegative == true)
+
+            {
+
+                return "負" + functionReturnValue;
+
+            }
+
+            else
+
+            {
+
+                return functionReturnValue;
+
+            }
+
+        }
+        #endregion
     }
 }
