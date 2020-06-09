2f1d13717cbf73c3df6ab20a68aa95a38ac943af
音訊科技 <mhu@speechmessage.com.tw>
2020-06-09 14:27:02 +0800

    ++++

--- ChurchReport/Models/QpayManager.cs
+++ ChurchReport/Models/QpayManager.cs
@@ -23,106 +23,6 @@ namespace ChurchReport.Models
         public Entity m_Contact;
         #endregion
         #region Line 單獨登入
-        public QpayModel SetQpayModel(String UserLineId)
-        {
-            try
-            {
-                Entity LineLoginContact= this.m_Contact = this.m_ToolUtilityClass.RetrieveContactByLineId(UserLineId);
-
-                // 全名
-                m_QpayModel.FullName = this.m_ToolUtilityClass.GetEntityStringAttribute(ref LineLoginContact, "fullname");
-                // 全名
-                m_QpayModel.Mobile = this.m_ToolUtilityClass.GetEntityStringAttribute(ref LineLoginContact, "mobilephone");
-                // 奉獻單編號
-                m_QpayModel.DedicationNumber = this.m_ToolUtilityClass.GetEntityStringAttribute(ref LineLoginContact, "pager");
-                // 身分證字號
-                m_QpayModel.NationId = this.m_ToolUtilityClass.GetEntityStringAttribute(ref LineLoginContact, "new_personal_id");
-
-                //奉獻類別
-                m_QpayModel.Category = "十一";
-                //付款方式
-                m_QpayModel.PayWay = "信用卡";
-                //奉獻日期
-                m_QpayModel.DedicationDate = DateTime.Now;
-                //奉獻分堂
-                m_QpayModel.DedicateLocation = "永和禮拜堂";
-
-                m_QpayModel.OtherCategoryArray = new List<String>();
-                EntityCollection TaskCollection = m_ToolUtilityClass.RetrieveTaskByFetchXml("宣道支持奉獻(請勿刪除)");
-                String Description = "";
-                if (TaskCollection.Entities.Count > 0)
-                {
-                    Description = this.m_ToolUtilityClass.GetEntityStringAttribute(TaskCollection.Entities[0], "description");
-                }
-
-                String[] OtherCategoryArray = Description.Split(',');
-                foreach (String OtherCategory in OtherCategoryArray)
-                {
-                    m_QpayModel.OtherCategoryArray.Add(OtherCategory);
-                }
-
-                if (m_QpayModel.CreditCardList == null)
-                {
-                    m_QpayModel.CreditCardList = new List<CreditCard>();
-                }
-                else
-                {
-                    m_QpayModel.CreditCardList.Clear();
-                }
-                // 處理信用卡清單
-                ProcessCreditCard();
-
-                m_QpayModel.QueryStartDate = new DateTime(DateTime.Now.Year, 1, 1);
-                m_QpayModel.QueryEndDate = DateTime.Now;
-
-                return m_QpayModel;
-            }
-            catch (System.Exception e)
-            {
-                String ErrorString = "ERROR : FullName = " + this.GetType().FullName.ToString() + " , Time = " + DateTime.Now.ToString() + " , Description = " + e.ToString();
-
-                //Monitor.Exit(this);
-                throw e;
-            }
-        }
-        public async Task<IActionResult> SaveQPayDedication(QpayModel QpayModel, String LineUserId)
-        {
-            try
-            {
-                if (QpayModel.Amount != null && QpayModel.Amount > 0)
-                {
-                    String DedicationResult = await m_QPayProcessor.CreateFeeAsync(LineUserId, QpayModel);
-
-                    String PayWay = "";
-                    if (DedicationResult.Contains("*** 請依照訊息付款 ***") != true)
-                    {
-                        PayWay = "信用卡";
-                    }
-                    else
-                    {
-                        PayWay = "虛擬帳號";
-                    }
-                    return Json(new { status = "1", message = "感謝您的奉獻", DedicationResult = DedicationResult, PayWay = PayWay });
-                }
-                else
-                {
-                    return Json(new { status = "2", message = "未輸入奉獻金額" });
-                }
-            }
-            catch (System.Exception e)
-            {
-                string ErrorString = "錯誤訊息 : FullName = " + GetType().FullName.ToString() + " , Time = " + DateTime.Now.ToString() + " , Description = " + e.ToString();
-                //m_ToolUtilityClass.TraceByLevel(TOTAL_LEVEL, LEVEL_1, ErrorString);
-
-                LineMessagingProcessorClass aLineMessagingProcessorClass = new LineMessagingProcessorClass();
-
-                aLineMessagingProcessorClass.SendMessage("U7638e4ed509708a3573ba6d69970583d", "永和禮拜堂 : 綁定錯誤 => " + ErrorString);
-
-                //return RedirectToAction("DisplayErrorView", new { ErrorMessage = e.Message });
-
-                throw e;
-            }
-        }
         public async Task<IActionResult> SaveKeyInDedication(QpayModel QpayModel)
         {
             try
@@ -363,48 +263,6 @@ namespace ChurchReport.Models
                 throw e;
             }
         }
-        private String ConvertToPayway(Entity aFeeEntity)
-        {
-            switch (this.m_ToolUtilityClass.GetOptionSetAttribute(aFeeEntity, "new_pay_way"))
-            {
-                case 100000004:
-                    return "未知";
-                case 100000000:
-                    return "現金";
-                case 100000001:
-                    return "信用卡";
-                case 100000002:
-                    return "ATM轉帳";
-                case 100000003:
-                    return "超商付款";
-                default:
-                    return "未知";
-            }
-        }
-        public String ConvertToCategory(Entity aFeeEntity)
-        {
-            switch (this.m_ToolUtilityClass.GetOptionSetAttribute(aFeeEntity, "new_category"))
-            {
-                case 100000000:
-                    return "十一";
-                case 100000001:
-                    return "感恩";
-                case 100000002:
-                    return "建堂";
-                case 100000003:
-                    return "宣教";
-                case 100000004:
-                    return "急難救助";
-                case 100000005:
-                    return "青年事工";
-                case 100000006:
-                    return "萬軍";
-                case 100000007:
-                    return "其他";
-                default:
-                    return "十一";
-            }
-        }
         #endregion
         #region 電腦網頁登入
         public QpayModel SetQpayModel(Entity aContact)
@@ -631,6 +489,50 @@ namespace ChurchReport.Models
                 string ErrorString = "錯誤訊息 : FullName = " + GetType().FullName.ToString() + " , Time = " + DateTime.Now.ToString() + " , Description = " + e.ToString();
 
                 throw e;
+            }
+        }
+        #endregion
+        #region 工具區
+        private String ConvertToPayway(Entity aFeeEntity)
+        {
+            switch (this.m_ToolUtilityClass.GetOptionSetAttribute(aFeeEntity, "new_pay_way"))
+            {
+                case 100000004:
+                    return "未知";
+                case 100000000:
+                    return "現金";
+                case 100000001:
+                    return "信用卡";
+                case 100000002:
+                    return "ATM轉帳";
+                case 100000003:
+                    return "超商付款";
+                default:
+                    return "未知";
+            }
+        }
+        public String ConvertToCategory(Entity aFeeEntity)
+        {
+            switch (this.m_ToolUtilityClass.GetOptionSetAttribute(aFeeEntity, "new_category"))
+            {
+                case 100000000:
+                    return "十一";
+                case 100000001:
+                    return "感恩";
+                case 100000002:
+                    return "建堂";
+                case 100000003:
+                    return "宣教";
+                case 100000004:
+                    return "急難救助";
+                case 100000005:
+                    return "青年事工";
+                case 100000006:
+                    return "萬軍";
+                case 100000007:
+                    return "其他";
+                default:
+                    return "十一";
             }
         }
         #endregion
