3e7071d8c1f0a316a06a26089b4d591f2939cf81
音訊科技 <mhu@speechmessage.com.tw>
2021-04-02 08:14:03 +0800

    \\\\

--- ChurchReport/WebServiceConnector/QPayProcessor.cs
+++ ChurchReport/WebServiceConnector/QPayProcessor.cs
@@ -30,9 +30,9 @@ namespace ChurchReport.WebServiceConnector
         #region 資料區
         // 商店編號
         // SANDBOX 測試用
-        string m_ShopNo = "NA0149_001";
+        //string m_ShopNo = "NA0149_001";
         // 永豐金流正式環境
-        //string m_ShopNo = "DA1626_001";
+        string m_ShopNo = "DA2424_001";
 
         // 使用 LineMessageApi WebHook
         //private const String RETURN_URL = "https://yhchurchback.speechmessage.com.tw:454/api/QPayCard/QPayReturnUrl";
