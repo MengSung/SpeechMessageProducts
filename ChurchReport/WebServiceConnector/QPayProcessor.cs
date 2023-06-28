ff5861559fa30f7090327f3cccd078d2e3a1014c
音訊科技 <mengsunghu@gmail.com>
2023-06-28 15:30:53 +0800

    +++++++

--- ChurchReport/WebServiceConnector/QPayProcessor.cs
+++ ChurchReport/WebServiceConnector/QPayProcessor.cs
@@ -37,12 +37,12 @@ namespace ChurchReport.WebServiceConnector
 
         #region 公司內部開發
         // 使用 ChurchReport 當作 WebHook
-        //private const String RETURN_URL = "https://fbllcback.speechmessage.com.tw:460/api/QPayCard/QPayReturnUrl";
-        //private const String BACKEND_URL = "http://QPbackendback.speechmessage.com.tw/api/QPayAtm/QPayBackendUrl";// 公司內部開發
+        private const String RETURN_URL = "https://shekinah611back.speechmessage.com.tw:473/api/QPayCard/QPayReturnUrl";
+        private const String BACKEND_URL = "http://QPbackendback.speechmessage.com.tw/api/QPayAtm/QPayBackendUrl";// 公司內部開發
         #endregion
         #region 雲端機房
-        private const String RETURN_URL = "https://fbllc.speechmessage.com.tw:398/api/QPayCard/QPayReturnUrl";
-        private const String BACKEND_URL = "http://QPaybackend.speechmessage.com.tw/api/QPayAtm/QPayBackendUrl"; // 雲端機房
+        //private const String RETURN_URL = "https://fbllc.speechmessage.com.tw:398/api/QPayCard/QPayReturnUrl";
+        //private const String BACKEND_URL = "http://QPaybackend.speechmessage.com.tw/api/QPayAtm/QPayBackendUrl"; // 雲端機房
         #endregion
 
         // 客製化
@@ -65,7 +65,7 @@ namespace ChurchReport.WebServiceConnector
         //private DateTime m_AtmExpireDate;
 
         // 客製化
-        private const String QPAY_ORGANIZATION = "fbllc";
+        private const String QPAY_ORGANIZATION = "shekinah611back";
 
         #endregion
         #region 初始化
