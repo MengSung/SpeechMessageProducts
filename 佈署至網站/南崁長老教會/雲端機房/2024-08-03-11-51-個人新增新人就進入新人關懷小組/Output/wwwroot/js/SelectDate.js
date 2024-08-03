0f13b224fb37c89eafe680800718daa5cb7ca47f
音訊科技 <mhu@speechmessage.com.tw>
2024-08-03 12:04:08 +0800

    上船更新

---
+++ 佈署至網站/南崁長老教會/雲端機房/2024-08-03-11-51-個人新增新人就進入新人關懷小組/Output/wwwroot/js/SelectDate.js
@@ -0,0 +1,75 @@
+﻿    function OnValueChanged(arg) {
+
+        //debugger;
+
+        //alert("OnValueChanged : " + arg);
+        //alert("OnValueChanged : " + arg.value);
+
+        //alert("OnValueChanged : " + arg.attribute('text'));
+        //alert("OnValueChanged : " + text: arg.attribute("text"));
+
+        //alert( getODataLocalDateFilter(arg.value) );
+    }
+    function GridDateChanged() {
+
+        //debugger;
+
+        alert("GridDateChanged !");
+    //alert("OnValueChanged : " + arg.value);
+
+    //alert("OnValueChanged : " + arg.attribute('text'));
+    //alert("OnValueChanged : " + text: arg.attribute("text"));
+
+    //alert( getODataLocalDateFilter(arg.value) );
+    }
+
+        function notify(data) {
+            var buttonText = data.component.option("text");
+            DevExpress.ui.notify("The " + buttonText + " button was clicked");
+        }
+
+        function getODataLocalDateFilter(date) {
+
+            //debugger;
+            if (date == null) {
+                //date = Date.now();
+                date = new Date();
+                //return ;
+            }
+
+            //alert("getODataLocalDateFilter !" + date);
+            //alert("HELLO 我愛嘟嘟妞妞 006!" + date);
+
+            //-- Description: For converting the date object to local time format
+            //-- You can also convert this to UTC Date format
+            //-- UTC Usage: getUTCMonth(), getUTCFullYear(), getUTCHours() ...
+
+            var monthString;
+            var rawMonth = (date.getMonth() + 1).toString();
+            if (rawMonth.length == 1) {
+        monthString = "0" + rawMonth;
+    }
+            else {monthString = rawMonth; }
+
+            var dateString;
+            //var rawDate = date.getUTCDate().toString();
+            var rawDate = date.getDate().toString();
+            if (rawDate.length == 1) {
+        dateString = "0" + rawDate;
+    }
+            else {dateString = rawDate; }
+
+
+            var DateFilter = "";
+            //DateFilter += date.getFullYear() + "-";
+            //DateFilter += monthString + "-";
+            //DateFilter += dateString;
+            //DateFilter += " T" + date.getHours() + ":";
+            //DateFilter += date.getMinutes() + ":";
+            //DateFilter += date.getSeconds() + ":";
+            //DateFilter += date.getMilliseconds();
+
+            //return DateFilter;
+
+            return date.getFullYear() + "/" + rawMonth + "/" + rawDate;
+        }
