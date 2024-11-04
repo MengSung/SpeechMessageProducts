733a0790c6dabb15a301e85d5c1b48108be9c82f
音訊科技 <mhu@speechmessage.com.tw>
2024-11-04 12:00:09 +0800

    移除、登入、設定帳號密碼按鈕

---
+++ 佈署至網站/迦南長老教會/雲端機房/2024-10-29-15-02-修正奉獻清單的付款方式對準後台/Output/wwwroot/js/TreeView.js
@@ -0,0 +1,20 @@
+﻿function OnItemSelectionChanged(args) {
+    //alert("AAA");
+    var value = args.component.getSelectedNodesKeys();
+    //alert("value =" + value);
+    //alert("args.component = " + args.component);
+    component.option("value", value);
+}
+
+function OptionChanged(args) {
+    //alert("BBB");
+}
+
+
+function ContentReady(args) {
+    //debugger;
+    //alert("ContentReady 001");
+    var Option = component.option("value");
+    //alert("Option = " + Option);
+    syncTreeViewSelection(args.component, component.option("value"));
+}
