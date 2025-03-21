ca952a0a566daffc9259827b1bd5b09c28995672
音訊科技 <mhu@speechmessage.com.tw>
2025-03-21 10:49:38 +0800

    整理ClassName的課程名稱

--- ChurchReport/Models/ClassName.cs
+++ ChurchReport/Models/ClassName.cs
@@ -10,21 +10,36 @@ namespace ChurchReport.Models {
         public ClassName()
         { }
 
-        public string Lesson1 { get; set; } = "第一課";
-        public string Lesson2 { get; set; } = "第二課";
-        public string Lesson3 { get; set; } = "第三課";
-        public string Lesson4 { get; set; } = "第四課";
-        public string Lesson5 { get; set; } = "第五課";
-        public string Lesson6 { get; set; } = "第六課";
-        public string Lesson7 { get; set; } = "第七課";
-        public string Lesson8 { get; set; } = "第八課";
-        public string Lesson9 { get; set; } = "第九課";
+        public string Lesson1 { get; set; }   = "第一課";
+        public string Lesson2 { get; set; }   = "第二課";
+        public string Lesson3 { get; set; }   = "第三課";
+        public string Lesson4 { get; set; }   = "第四課";
+        public string Lesson5 { get; set; }   = "第五課";
+        public string Lesson6 { get; set; }   = "第六課";
+        public string Lesson7 { get; set; }   = "第七課";
+        public string Lesson8 { get; set; }   = "第八課";
+        public string Lesson9 { get; set; }   = "第九課";
         public string Lesson10 { get; set; }  = "第十課";
         public string Lesson11 { get; set; }  = "第十一課";
         public string Lesson12 { get; set; }  = "第十二課";
         public string Lesson13 { get; set; }  = "第十三課";
         public string Lesson14 { get; set; }  = "第十四課";
         public string Lesson15 { get; set; }  = "第十五課";
+        public string Lesson16 { get; set; }  = "第十六課";
+        public string Lesson17 { get; set; }  = "第十七課";
+        public string Lesson18 { get; set; }  = "第十八課";
+        public string Lesson19 { get; set; }  = "第十九課";
+        public string Lesson20 { get; set; }  = "第二十課";
+        public string Lesson21 { get; set; }  = "第二十一課";
+        public string Lesson22 { get; set; }  = "第二十二課";
+        public string Lesson23 { get; set; }  = "第二十三課";
+        public string Lesson24 { get; set; }  = "第二十四課";
+        public string Lesson25 { get; set; }  = "第二十五課";
+        public string Lesson26 { get; set; }  = "第二十六課";
+        public string Lesson27 { get; set; }  = "第二十七課";
+        public string Lesson28 { get; set; }  = "第二十八課";
+        public string Lesson29 { get; set; }  = "第二十九課";
+        public string Lesson30 { get; set; }  = "第三十課";
         public string HomeWorkA { get; set; } = "作業A";
         public string HomeWorkB { get; set; } = "作業B";
         public string HomeWorkC { get; set; } = "作業C";
