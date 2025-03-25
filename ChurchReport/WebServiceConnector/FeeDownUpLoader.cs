3f4a8ad89a4e033a64bdb1765461e9dd8dd7755c
音訊科技 <mhu@speechmessage.com.tw>
2025-03-25 11:15:33 +0800

    修正GetDiscipleLessonsEntityCollection

--- ChurchReport/WebServiceConnector/FeeDownUpLoader.cs
+++ ChurchReport/WebServiceConnector/FeeDownUpLoader.cs
@@ -171,21 +171,22 @@ namespace ChurchReport.WebServiceConnector
             EntityCollection aDiscipleLessonsEntity = new EntityCollection();
             // 是否是講員
             EntityCollection aDiscipleLessonsEntityCollection = m_ToolUtilityClass.QueryEntityListByDate("contact", "contactid", this.m_ContactEntity.Id.ToString(), "new_contact_teacher_new_disciple_less", "new_disciple_lessons");
-            if (aDiscipleLessonsEntityCollection.Entities.Count > 0)
+            for( int i =0; i < aDiscipleLessonsEntityCollection.Entities.Count;i ++)
             {
-                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[0]);
+                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[i]);
             }
             // 是否是收費點名人員
             aDiscipleLessonsEntityCollection = m_ToolUtilityClass.QueryEntityListByDate("contact", "contactid", this.m_ContactEntity.Id.ToString(), "new_contact_new_disciple_lessons_fee", "new_disciple_lessons");
-            if (aDiscipleLessonsEntityCollection.Entities.Count > 0)
+            for (int i = 0; i < aDiscipleLessonsEntityCollection.Entities.Count; i++)
             {
-                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[0]);
+                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[i]);
             }
+
             // 是否是收費點名人員助理
             aDiscipleLessonsEntityCollection = m_ToolUtilityClass.QueryEntityListByDate("contact", "contactid", this.m_ContactEntity.Id.ToString(), "new_contact_new_disciple_lessons_assi", "new_disciple_lessons");
-            if (aDiscipleLessonsEntityCollection.Entities.Count > 0)
+            for (int i = 0; i < aDiscipleLessonsEntityCollection.Entities.Count; i++)
             {
-                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[0]);
+                aDiscipleLessonsEntity.Entities.Add(aDiscipleLessonsEntityCollection.Entities[i]);
             }
 
             return aDiscipleLessonsEntity;
