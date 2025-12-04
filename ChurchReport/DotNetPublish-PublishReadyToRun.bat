8e8334c09cb663c5b63f3c066a518bf2ea7d5221
Administor <mengsunghu@gmial.com>
2025-12-04 11:10:38 +0800

    <ServerGarbageCollection>true</ServerGarbageCollection>

--- ChurchReport/DotNetPublish-PublishReadyToRun.bat
+++ ChurchReport/DotNetPublish-PublishReadyToRun.bat
@@ -4,7 +4,11 @@ dotnet publish -c Release -r win-x64 --self-contained false ^
   /p:PublishReadyToRun=true ^
   /p:PublishTrimmed=false ^
   /p:PublishAot=false ^
+  /p:PublishReadyToRunComposite=true ^
+  /p:TieredPGO=true ^
+  /p:OptimizationPreference=Speed ^
   /p:DebugType=None ^
+  /p:DebugSymbols=false ^
   -o "./bin/Output-Release-PublishReadyToRun"
 
 pause
